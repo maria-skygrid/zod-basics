64e0e2b1d2505e425c6f691b443c88a14b64d965
maria-skygrid <146316981+maria-skygrid@users.noreply.github.com>
2023-10-21 16:16:52 +0900

    Basic userSchema validation

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,16 @@
-console.log("hello world");
-console.log("wewe");
+import { z } from "zod"
+
+//期待している値
+const userSchema = z.object({
+  email: z.string().email(),
+  userName: z.string(),
+  phone: z.number(),
+})
+
+//ユーザー入力と比較
+userSchema.parse({
+  email: "mmm00@gmal.com",
+  userName: "12ma",
+  phone: 234, 
+  age: 30
+})
